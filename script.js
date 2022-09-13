65fb15c017c9d5f5d32e070f545dc934351e8f40
Chetan kumar <chetankumar71297@gmail.com>
2022-09-13 00:05:03 -0700

    Revisiting Rock Paper Scissors
    UI is added with some change in code.Know first to score 5 points
    will win.

--- script.js
+++ script.js
@@ -10,15 +10,66 @@ function getComputerChoice() {
 
     return computer_guess;
 }
+
+// both variables are created to record number of wins by each
 let computer_wins = 0;
 let player_wins = 0;
+
+// restart button is used to rest the scores and will appear when anyone wins
+const restart = document.createElement('button');
+restart.textContent = "Restart";
+restart.addEventListener('click', () => reset());
+
+const body = document.querySelector('body');
+
+const buttons = document.querySelectorAll('button');
+
+// this step will assign inner text of buttons to player_guess var.
+buttons.forEach((btn) => {btn.addEventListener('click',() => player_choice(btn))});
+
+function player_choice(btn) {
+    let player_guess;
+    player_guess = btn.innerHTML;
+    if (computer_wins < 5 && player_wins < 5) {
+        let computer_guess = getComputerChoice();
+        let mid_result = playRound(player_guess, computer_guess);
+        document.getElementById("demo1").textContent = mid_result;
+        document.getElementById("demo2").textContent = `Your score: ${player_wins}  Computer score: ${computer_wins}`;
+    }  
+    
+    if (computer_wins == 5) {
+        document.getElementById("demo3").textContent = "You lost the game.Click restart to play again.";
+        
+        // Add restart button
+        body.appendChild(restart);
+    } 
+    
+    if (player_wins == 5) {
+        document.getElementById("demo3").textContent = "You wins the game.Click restart to play again.";
+        body.appendChild(restart);
+    }
+}
+    // it will reset the score so that game can be played again
+function reset(){    
+    
+        computer_wins = 0;
+        player_wins = 0;
+        body.removeChild(restart);
+        document.getElementById("demo1").textContent = '';
+        document.getElementById("demo2").textContent = `Your score: ${player_wins}  Computer score: ${computer_wins}`;
+        document.getElementById("demo3").textContent = '';
+}
+
+
+
+
 // take user input as a string from rock,paper and scissors
 // make a function which take computer guess and user guess as parameters to decide a winner
 function playRound(player_guess, computer_guess) {
-    // both variables are created to record number of wins by each
     
-    player_guess = player_guess.charAt(0).toUpperCase() 
-                   + player_guess.slice(1).toLowerCase(); // convert ROCK,rock etc to Rock
+    // IGNORE THIS COMMENT!
+    // player_guess = player_guess.charAt(0).toUpperCase() 
+    //               + player_guess.slice(1).toLowerCase(); // convert ROCK,rock etc to Rock
 
     // if (computer_guess == user_guess)--> It's a tie
     if (computer_guess == player_guess) {
@@ -37,27 +88,24 @@ function playRound(player_guess, computer_guess) {
     // else --> user wins
   }
    
-  //let player_guess = "PAPER";
-  //
-  //console.log(computer_guess);
   
-
+// IGNORE THESE COMMENTS!
 // Develop this game into 5 rounds,who wins most rounds will be the winner
-function game() {
+// function game() {
     
-    for (let i = 0; i < 5; i++) {
-        let player_guess = prompt("Make your choice from rock,paper and scissors");
-        const computer_guess = getComputerChoice();
-        console.log(playRound(player_guess, computer_guess));
-    }
-        if (player_wins > computer_wins) {
-            console.log(`Hurry,you win's by ${player_wins-computer_wins} games out of 5 games!`);
-        } else if (player_wins < computer_wins) {
-            console.log(`You loose by ${computer_wins-player_wins} games out of 5 games`);
-        } else {
-            console.log(`Game Draw!! you and computer both wins equal games`);
-        }
+//     for (let i = 0; i < 5; i++) {
+//         let player_guess = prompt("Make your choice from rock,paper and scissors");
+//         const computer_guess = getComputerChoice();
+//         console.log(playRound(player_guess, computer_guess));
+//     }
+//         if (player_wins > computer_wins) {
+//             console.log(`Hurry,you win's by ${player_wins-computer_wins} games out of 5 games!`);
+//         } else if (player_wins < computer_wins) {
+//             console.log(`You loose by ${computer_wins-player_wins} games out of 5 games`);
+//         } else {
+//             console.log(`Game Draw!! you and computer both wins equal games`);
+//         }
     
-}
+// }
 
 
